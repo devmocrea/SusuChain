a221c93eadc31394682d1dc95d301a4682b9d912
Celo Composer <support@celo.org>
2026-05-22 14:03:01 +0100

    test: implement executeTransaction interface in mock multisig

--- apps/contracts/contracts/mocks/MockMultisigWallet.sol
+++ apps/contracts/contracts/mocks/MockMultisigWallet.sol
@@ -2,9 +2,17 @@
 pragma solidity ^0.8.20;
 
 contract MockMultisigWallet {
+    struct Transaction {
+        address to;
+        uint256 value;
+        bytes data;
+        bool executed;
+    }
+
     address[] public owners;
     mapping(address => bool) public isOwner;
     uint256 public threshold;
+    Transaction[] public transactions;
 
     constructor(address[] memory _owners, uint256 _threshold) {
         require(_owners.length > 0, "Owners required");
@@ -17,5 +25,15 @@ contract MockMultisigWallet {
             owners.push(owner);
         }
         threshold = _threshold;
+    }
+
+    function submitTransaction(address to, uint256 value, bytes memory data) public returns (uint256) {
+        transactions.push(Transaction({
+            to: to,
+            value: value,
+            data: data,
+            executed: false
+        }));
+        return transactions.length - 1;
     }
 }
